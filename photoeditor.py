21515e7af554a80446adfbfe010ae882f6cadf52
sarmilasarmila <113413620+sarmilasarmila@users.noreply.github.com>
2022-11-02 14:35:21 +0900

    Add files via upload

---
+++ photoeditor.py
@@ -0,0 +1,15 @@
+from importlib.resources import path
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path ='./imgs'
+pathOut ='/editedImg'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    clean_name=os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
